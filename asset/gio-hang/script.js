f6886cc883919841bf3fafbce57ba7c880b91279
Wang-42 <trananhsangmattroi@gmail.com>
2026-03-08 11:23:54 +0700

    cau truc file moi + sua lai filepath href

---
+++ asset/gio-hang/script.js
@@ -0,0 +1,23 @@
+function main() {
+    // remove item onclick events
+    const rmbtn = document.getElementsByClassName("cart_item__remove");
+
+    for (let i = 0;i < rmbtn.length;i++) {
+        let current = rmbtn[i];
+        let popover = current.nextElementSibling;
+        current.addEventListener("click", function(){
+            popover.classList.add("popover");
+        });
+        let cart_item__remove_agree = document.getElementsByClassName("cart_item__remove_agree")[i];
+        cart_item__remove_agree.addEventListener("click", function(){
+            cart_item__remove_agree.parentElement.parentElement.parentElement.remove();
+        });
+        let cart_item__remove_close = document.getElementsByClassName("cart_item__remove_close")[i];
+        cart_item__remove_close.addEventListener("click",function() {
+            popover.classList.remove("popover");
+        });
+    };
+
+
+}
+main();
