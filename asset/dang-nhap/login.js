41dba25fcc18244957209afad303779c0785b608
Murasame2301 <trungtinlenguyen231@gmail.com>
2026-03-27 20:46:14 +0700

    update

--- asset/dang-nhap/login.js
+++ asset/dang-nhap/login.js
@@ -1,24 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.querySelector('.login-form'); // Giả sử class form là .login-form
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.querySelector(".login-form"); // Giả sử class form là .login-form
 
-    if (loginForm) {
-        loginForm.addEventListener('submit', function (e) {
-            e.preventDefault();
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-            const userInp = document.getElementById('loginUser').value.trim();
-            const passInp = document.getElementById('loginPass').value;
+      const userInp = document.getElementById("loginUser").value.trim();
+      const passInp = document.getElementById("loginPass").value;
+      const loginError = document.getElementById("loginError");
 
-            const accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+      const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
 
-            const validUser = accounts.find(acc => acc.userName === userInp && acc.password === passInp);
+      const validUser = accounts.find(
+        (acc) => acc.userName === userInp && acc.password === passInp,
+      );
 
-            if (validUser) {
-                alert("Đăng nhập thành công! Chào mừng " + validUser.fullName);
-                sessionStorage.setItem('currentUser', JSON.stringify(validUser));
-                window.location.href = "./index.html"; 
-            } else {
-                alert("Tên đăng nhập hoặc mật khẩu không chính xác!");
-            }
-        });
-    }
+      if (validUser) {
+        alert("Đăng nhập thành công! Chào mừng " + validUser.fullName);
+        sessionStorage.setItem("currentUser", JSON.stringify(validUser));
+        window.location.href = "./index.html";
+      } else {
+        loginError.style.display = "block";
+      }
+    });
+  }
 });
